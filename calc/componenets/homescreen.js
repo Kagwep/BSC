6f71ebef46665e061d6157fdc43a77925bc4c0de
Kagwep <kagwepeter07@gmail.com>
2022-09-28 00:29:34 +0300

    c11

---
+++ calc/componenets/homescreen.js
@@ -0,0 +1,93 @@
+import React, {Component} from "react";
+import { TouchableOpacity, View,Text,Alert } from "react-native";
+
+
+
+export default function homescreen(){
+    return (
+
+    <View style={{flex:1,borderWidth:2,marginTop:25,flexDirection:"column"}}>
+        <View style={{flex:1,borderWidth:1,borderColor:"black",flexDirection:'row',justifyContent:"space-around",padding:2,alignItems:"center"}}>
+          <View style={{borderColor:"green",borderWidth:1,flex:1,padding:2,margin:2,height:50+"%",backgroundColor:"blue"}}> 
+            <Text > icon</Text>
+          </View>
+          <View style={{borderColor:"green",borderWidth:1,flex:9,borderRadius:2,height:50+"%",justifyContent:'flex-start',alignItems:"center",flexDirection:"row"}}> 
+            <Text > search products</Text>
+          </View>
+        </View>
+        <View style={{flex:1.5,borderWidth:1,borderColor:"black",flexDirection:"row",alignItems:"center",justifyContent:"flex-start"}}>
+          <View style={{width:20+"%",height:80+"%",borderWidth:1,padding:2,margin:2,borderRadius:5,backgroundColor:"brown",flexDirection:"column"}}>
+            
+            <View style={{height:60+"%",justifyContent:"center",alignItems:"center"}}>
+              <Text> IC </Text>
+            </View>
+            <View style={{height:30+"%"}}>
+            <Text> products</Text>
+            </View>
+            
+          </View>
+          <View style={{width:20+"%",height:80+"%",borderWidth:1,padding:2,margin:2,borderRadius:5,backgroundColor:"magenta"}}>
+          <View style={{height:60+"%",justifyContent:"center",alignItems:"center"}}>
+              <Text> IC </Text>
+            </View>
+            <View style={{height:30+"%"}}>
+            <Text> rep</Text>
+            </View>
+          </View>
+          <View style={{width:20+"%",height:80+"%",borderWidth:1,padding:2,margin:2,borderRadius:5,backgroundColor:"brown"}}>
+          <View style={{height:60+"%",justifyContent:"center",alignItems:"center"}}>
+              <Text> IC </Text>
+            </View>
+            <View style={{height:30+"%"}}>
+            <Text> products</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{flex:1,borderWidth:1,borderColor:"black",flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}> 
+          <TouchableOpacity style={{margin:5,backgroundColor:"red",padding:5,borderRadius:3}} > 
+            <Text> new products</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{margin:5}}> 
+            <Text style={{color:"orange"}}> see all</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex:3,borderWidth:1,borderColor:"black",flexDirection:"row",justifyContent:"space-between",alignItems:"center"}}>
+          <View style={{flex:1,borderWidth:1,height:95+"%",flexDirection:"column",margin:2,borderRadius:5,alignItems:"center",justifyContent:"space-between"}}>
+            <View style={{height:50+"%",borderRadius:5}}>
+               <Text> image </Text>
+               
+            </View>
+            <View style={{height:30+"%"}}>
+               <Text> desc </Text>
+               
+            </View>
+            <View style={{height:20+"%"}}> 
+              <Text> price </Text>
+              
+            </View>
+          </View>
+          <View style={{flex:1,borderWidth:1,height:95+"%",flexDirection:"column",margin:2,borderRadius:5,alignItems:"center",justifyContent:"space-between"}}>
+            <View>
+               <Text> image </Text>
+               
+            </View>
+            <View>
+               <Text> desc </Text>
+               
+            </View>
+            <View> 
+              <Text> price </Text>
+              
+            </View>
+          </View>
+        </View>
+        <View style={{flex:4.5,borderWidth:1,borderColor:"black",margin:2}}>
+          <View style={{margin:2,alignItems:"center"}}>
+            <Text> box </Text>
+
+          </View>
+
+        </View>
+      </View>
+    );
+}
---
+++ calc/navigation/maintabnavigator.js
@@ -0,0 +1,43 @@
+// screens
+import { View } from "react-native";
+import homescreen from "../componenets/homescreen";
+
+export function wishlistscreen(){
+
+    return(
+        <View>
+            <Text>
+                wishlist screen
+            </Text>
+        </View>
+    )
+}
+
+export function searchscreen(){
+
+    return(
+        <View>
+            <Text>
+                search screen
+            </Text>
+        </View>
+    )
+}
+
+
+export function proilescreen(){
+
+    return(
+        <View>
+            <Text>
+                profile screen
+            </Text>
+        </View>
+    )
+}
+// stacks 
+// tab navigator
+
+
+
+//export appcontainer
